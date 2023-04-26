58f172581c25ce4baed189f6ae8e64e0d796100b
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-04-26 13:51:34 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    // Use Enter to submit answer
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -23,6 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed 
  */
 function runGame(gameType) {
+    // Empty answer box
+    document.getElementById("answer-box").value = "";
+    // Curser is in the answer box
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
